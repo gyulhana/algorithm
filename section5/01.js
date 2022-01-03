a8f09197d738d8e9961548382bd80ebdb8fb0eae
gyulhana <matearz@korea.ac.kr>
2022-01-03 10:56:09 +0900

    📝 add: 투포인터 알고리즘 풀이

--- section5/01.js
+++ section5/01.js
@@ -1,5 +1,20 @@
 function solution(arr1, arr2) {
-  return [...arr1, ...arr2].sort();
+  // return [...arr1, ...arr2].sort(); // nlogn sort()는 속도 느림
+  // 투포인터 알고리즘
+  const answer = [];
+  const n = arr1.length;
+  const m = arr2.length;
+
+  let p1 = 0;
+  let p2 = 0;
+
+  while (p1 < n && p2 < m) {
+    if (arr1[p1] <= arr2[p2]) answer.push(arr1[p1++]);
+    else answer.push(arr2[p2++]);
+  }
+  while (p1 < n) answer.push(arr1[p1++]);
+  while (p2 < m) answer.push(arr2[p2++]);
+  return answer;
 }
 
 console.log(solution([1, 3, 5], [2, 3, 6, 7, 9]));
